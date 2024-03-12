ee5ab7b4cb771367550e26af4a33d0264df7e25e
AE06909 <wes.gonzales@elevancehealth.com>
2024-03-12 08:36:06 -0400

    Logout functionality.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 
 from db import db
+from blocklist import BLOCKLIST
 import models
 
 from resources.item import blp as ItemBlueprint
@@ -13,47 +14,81 @@ from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
 from resources.user import blp as UserBlueprint
 
+
 def create_app(db_url=None):
     app = Flask(__name__)
     CORS(app)
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
-    app.config["API_TITLE"] = 'Stores REST API'
-    app.config["API_VERSION"] = 'v1'
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["OPENAPI_SWAGGER_UI_URL"] = (
+        "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    )
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
+        "DATABASE_URL", "sqlite:///data.db"
+    )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
 
     api = Api(app)
 
+    app.config["JWT_SECRET_KEY"] = (
+        "JgZ+UrtOKhKW9bwBY9Sz0fKabfiJB26+tEKflvDOmklsNLHDWNXW5/453eJUICjXIXw0yw+f1BT0w9Do2wbs5Q=="
+    )
     jwt = JWTManager(app)
+
+    # Token revocation for logout.
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        # If true request is terminated, the token has been revoked.
+        return jwt_payload['jti'] in BLOCKLIST
+
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "[-] The token has been revoked.", "error": "token_revoked"}
+            ),
+            401
+        )        
+    # Token revocation for logout.
 
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
         return (
-            jsonify({"message": "[!] The token has expired.", "error": "token_expired"}),
+            jsonify(
+                {"message": "[!] The token has expired.", "error": "token_expired"}
+            ),
             401,
         )
 
     @jwt.invalid_token_loader
     def invalid_token_callback(error):
         return (
-            jsonify({"message": "[!] Signature verification failed.", "error": "invalid_token"}),
+            jsonify(
+                {
+                    "message": "[!] Signature verification failed.",
+                    "error": "invalid_token",
+                }
+            ),
             401,
         )
 
     @jwt.unauthorized_loader
     def missing_token_callback(error):
         return (
-            jsonify({
-                "description": "[!] The request does not contain an access token.",
-                "error": "authorization_required",
-            }),
+            jsonify(
+                {
+                    "description": "[!] The request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
             401,
         )
 
